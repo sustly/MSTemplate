696af2dd5511a8084a9891004ed6b58db7fe504d
sustly <279719732@qq.com>
2019-04-26 17:17:04 +0800

    mysql

--- src/main/java/com/sustly/mysql/mst.sql
+++ src/main/java/com/sustly/mysql/mst.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50711
+Source Server         : yun
+Source Server Version : 80016
 Source Host           : localhost:3306
 Source Database       : mst
 
 Target Server Type    : MYSQL
-Target Server Version : 50711
+Target Server Version : 80016
 File Encoding         : 65001
 
-Date: 2019-03-21 10:17:05
+Date: 2019-04-26 17:16:01
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,8 +32,9 @@ CREATE TABLE `menu` (
 -- Records of menu
 -- ----------------------------
 INSERT INTO `menu` VALUES ('0', '系统菜单', 'icon-sys', '', null);
-INSERT INTO `menu` VALUES ('100', '测试', 'icon-sys', null, '0');
-INSERT INTO `menu` VALUES ('200', '测试2', 'icon-sys', null, '100');
+INSERT INTO `menu` VALUES ('100', '系统管理', 'icon-sys', null, '0');
+INSERT INTO `menu` VALUES ('101', '模块管理', 'icon-sys', 'menu', '100');
+INSERT INTO `menu` VALUES ('102', '用户管理', 'icon-sys', 'user', '100');
 
 -- ----------------------------
 -- Table structure for user
@@ -49,7 +50,7 @@ CREATE TABLE `user` (
   `department_code` char(32) DEFAULT NULL COMMENT '所属部门的编号',
   `department_name` char(32) DEFAULT NULL COMMENT '所属部门的名字',
   `login_name` char(32) NOT NULL COMMENT '登录所用的用户名',
-  `is_available` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否可用（默认为1），1-可用，0-不可用',
+  `is_available` int(11) NOT NULL DEFAULT '1' COMMENT '是否可用（默认为1），1-可用，0-不可用',
   `img_path` char(255) DEFAULT NULL COMMENT '头像图片的地址（前端文件）',
   `last_login_time` char(32) DEFAULT NULL COMMENT '最后一次登录的时间',
   `create_time` char(32) DEFAULT NULL COMMENT '该用户创建的时间',
@@ -61,4 +62,4 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '张三', '3f509f0f8508992b90675d74568d6958', null, null, null, null, null, 'system', '1', null, null, null, null);
+INSERT INTO `user` VALUES ('1', '王五', '3f509f0f8508992b90675d74568d6958', '123%252540163.com', '123456', null, null, null, 'system', '1', null, '2019-04-04 11:15:20', null, null);
