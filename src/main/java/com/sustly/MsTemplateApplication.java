292baf687828bf406e7f9dfdc4af912b71425f8c
sustly <279719732@qq.com>
2019-05-05 17:36:29 +0800

    config

--- src/main/java/com/sustly/MsTemplateApplication.java
+++ src/main/java/com/sustly/MsTemplateApplication.java
@@ -2,15 +2,21 @@ package com.sustly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author admin
  */
 @SpringBootApplication
-public class MsTemplateApplication {
+public class MsTemplateApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MsTemplateApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
